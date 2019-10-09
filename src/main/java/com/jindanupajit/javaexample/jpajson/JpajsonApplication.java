a00f3297435023850eae704c94d3b379f810f00d
Krissada Jindanupajit <jindanupajit@gmail.com>
2019-10-08 23:11:06 -0400

    Skeleton

---
+++ src/main/java/com/jindanupajit/javaexample/jpajson/JpajsonApplication.java
@@ -0,0 +1,13 @@
+package com.jindanupajit.javaexample.jpajson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpajsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpajsonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jindanupajit/javaexample/jpajson/JpajsonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jindanupajit.javaexample.jpajson;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JpajsonApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
